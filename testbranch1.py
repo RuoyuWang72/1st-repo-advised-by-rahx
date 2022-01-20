add13ddf3d0d9b5864f5b999bf49bedf405ab7e5
Rahmat Ashari <rahx@utexas.edu>
2022-01-20 00:18:00 -0600

    yeet

--- testbranch1.py
+++ testbranch1.py
@@ -1,1 +1,11 @@
-
+def bruhMoment():
+    shout = 1
+    for i in range(10):
+        if shout > 0:
+            print("BRUH ")
+        else:
+            print("bruh ")
+        shout *= -1
+        
+if __name__ == "__main__":
+    bruhMoment()
